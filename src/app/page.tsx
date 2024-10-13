8f7f9c96f267b8a4105a50e10b581c7b5ffccc0b
verrakav <verrakav@users.noreply.github.com>
2024-10-13 16:45:43 +1100

    cleaned up all console.logs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,30 +42,21 @@ const App = () => {
   const [queues, setQueues] = useState<Queue[]>(initialQueues);
   const [players, setPlayers] = useState<Player[]>(playersUpdated);
 
+  // REVIEW: rename to handleAddToShortestQueue ?
   const addItemToShortestQueue = async (itemId: string) => {
     // Find the item based on itemId
     const itemToUpdate = players.find(player => player.id === itemId);
 
-    // console.log(itemToUpdate);
-    // this is a tempQ item
-    // console.log(itemId);
-
     if (!itemToUpdate) {
-      console.error("Item not found");
-      return;
+      throw new Error("Item not found");
     }
 
     const shortestQueue = findShortestQueue(queues);
-    // console.log(shortestQueue);
 
-    // Create a new object with assignedToQueue set to true
     const updatedItem = {
       ...itemToUpdate,
       assignedToQueue: true
     };
-
-    // console.log("UPDATED ITEM");
-    // console.log(updatedItem);
 
     shortestQueue.queueItems.push(updatedItem);
 
@@ -76,40 +67,24 @@ const App = () => {
       return queue;
     });
 
-    // console.log("NEW QUEUES");
-    // console.log(newQueues);
-
     setPlayers(prevPlayers =>
       prevPlayers.map(player => (player.id === itemId ? updatedItem : player))
     );
-    // console.log("NEW PLAYERS");
-    // console.log(players);
-
-    // do the same as with Players?
-    await setQueues(newQueues);
+    setQueues(newQueues);
   };
 
+  // REVIEW: rename to handleAddToAllQueues ?
   // don't need async await?
   async function addAllToQueues(items: Player[]) {
-    //get how many items in a queue
-    // const totalItems = items.length;
-    console.log("these are items:", items);
-    // for (let i = 0; i < totalItems; i++) {
-    // const newItems = items.map(async item => {
     for (const item of items) {
       if (!item.assignedToQueue) {
-        // doesn't update the state properly
         item.assignedToQueue = true;
-        // console.log("SENDING ITEM TO QUEUE");
-        // console.log(item);
-        // console.log("CURRENT QUEUEs");
-        // console.log(queues);
         await addItemToShortestQueue(item.id);
       }
     }
-    // setPlayers(newItems);
   }
 
+  // REVIEW: helper
   const findShortestQueue = (queues: Queue[]) => {
     let shortestQueue = queues[0];
     queues.forEach(queue => {
@@ -119,21 +94,8 @@ const App = () => {
     });
     return shortestQueue;
   };
-  // Function to add all players to the queues
 
-  // const addAllToQueues = () => {
-  //   const newQueues = [...queues];
-  //   const totalItems = players.length;
-  //   let queueIndex = 0;
-
-  //   for (let i = 0; i < totalItems; i++) {
-  //     if (queueIndex === newQueues.length) queueIndex = 0;
-  //     addItemToShortestQueue(players[i], queueIndex);
-  //     queueIndex++;
-  //   }
-  // };
-
-  // Function to progress a queue one step
+  // REVIEW: rename to handleProgressOneStep ?
   const progressQueueOneStep = (queueIndex: number) => {
     const newQueues = [...queues];
     const processedPlayer: Player | undefined =
@@ -152,66 +114,22 @@ const App = () => {
     setQueues(newQueues);
   };
 
-  // REVIEW: potential trouble with state update
-  //seems to work | candidate to go into utils file
-  // function queueSlicer(queues) {
-  //   // console.log("these are queues entering queueSlicer", queues);
-  //   const shortestQueue = findShortestQueue(queues);
-  //   // console.log("these this is the shortest q", shortestQueue);
-
-  //   const shortestQLength = shortestQueue.queueItems.length;
-  //   // console.log(
-  //   //   "these this is the IDX to use to slice (shortest q length)",
-  //   //   shortestQLength
-  //   // );
-
-  //   const slicedQTailCollection = [];
-  //   const stumps = [];
-  //   for (let i = 0; i < queues.length; i++) {
-  //     slicedQTailCollection.push(queues[i].queueItems.slice(shortestQLength));
-  //     stumps.push(queues[i].queueItems.slice(0, shortestQLength));
-  //   }
-  //   console.log("SLICER stumps are: ", stumps);
-  //   console.log("SLICER slicedCollectios are: ", slicedQTailCollection);
-
-  //   return {stumps, slicedQTailCollection};
-  // }
-
-  // REVIEW:
+  // REVIEW: rename to handleRedistributeQueues ?
   function redestributeQueues(queues) {
-    console.log("these are queues entering redistributor", queues);
     const shortestQueue = findShortestQueue(queues);
-    console.log("these this is the shortest q", shortestQueue);
 
     const shortestQLength = shortestQueue.queueItems.length;
-    console.log(
-      "these this is the IDX to use to slice (shortest q length)",
-      shortestQLength
-    );
 
     const slicedQTailCollection = [];
     const stumps = [];
 
     for (let i = 0; i < queues.length; i++) {
-      console.log("queue at idx ", queues[i].queueItems);
       const slicedTail = queues[i].queueItems.slice(shortestQLength);
-      console.log("item to push ", slicedTail);
       slicedQTailCollection.push(slicedTail);
-      // works as expected
       stumps.push(queues[i].queueItems.slice(0, shortestQLength));
     }
-
-    console.log("stumps are: ", stumps);
-    console.log("slicedQTailCollection is: ", slicedQTailCollection);
-
     const tempQ = [];
-    // runs if there's someone left in the any of the slices
     while (slicedQTailCollection.some(q => q.length > 0)) {
-      console.log("WHILE LOOP");
-      console.log("SLICED Q TAIL COLLECTION");
-      console.log(slicedQTailCollection);
-      // for every sliceCollection if it's not empty run .shift & .push into tempQ
-      // for (let i = 0; i < slicedQTailCollection.length; i++) {
       slicedQTailCollection.forEach(tail => {
         if (tail.length > 0) {
           const itemToPush = tail.shift();
@@ -220,27 +138,10 @@ const App = () => {
       });
     }
 
-    //this is what we expect to have & in the right order
-    console.log("tempQ", tempQ);
-
-    // this make sure we update state correctly
-    // const newQueues = queues.map((queue, idx) => {
-    //   return {...queue, queueItems: stumps[idx]};
-    // });
-
-    // for (let i = 0; i < tempQ.length; i++) {
-    //   newQueues.forEach(queue => {
-    //     // if (!queue.queueItems.includes(tempQ[i])) queue.queueItems.push(tempQ[i]);
-    //     queue.queueItems.push(tempQ[i]);
-    //   });
-    // }
-    // add the tempQ to the stumps
     tempQ.forEach((qItem, index) => {
       const stumpIndex = index % stumps.length;
       stumps[stumpIndex].push(qItem);
     });
-
-    console.log("STUMPS FINAL - to be added to QUEUES ", stumps);
 
     const newQueues = queues.map((queue, index) => {
       return {...queue, queueItems: stumps[index]};
@@ -254,17 +155,14 @@ const App = () => {
       <h1>Queue Management</h1>
 
       <div className="p-8 bg-green-200">
-        {/* Header */}
         <header className="mb-8">
           <h2 className="text-2xl font-bold text-gray-700">Unprocessed Players</h2>
         </header>
 
-        {/* Grid of Player Cards */}
         <PlayersList
           players={players}
           addItemToShortestQueue={addItemToShortestQueue}
         />
-        {/* <Players players={players} addItemToShortestQueue={addItemToShortestQueue}/> */}
       </div>
       <div className="flex flex-row justify-around">
         <button
@@ -284,35 +182,30 @@ const App = () => {
       </div>
 
       <div className=" p-8 bg-blue-100">
-        {/* Header */}
         <header className="mb-8">
           <h2 className="text-3xl font-bold text-gray-700 text-center">Queues</h2>
         </header>
 
-        {/* Queues Grid */}
-        {/* COMPONENT: */}
         <QueuesGrid
           queues={queues}
           setQueues={setQueues}
+          //REVIEW: the player state & its setter aren't used in QueuesGrid
           players={players}
           setPlayers={setPlayers}
           onProgress={progressQueueOneStep}
         />
       </div>
 
-      {/* OLD: */}
       <div className="p-8 bg-yellow-200">
         {/* Header */}
         <header className="mb-8">
           <h2 className="text-2xl font-bold text-gray-700">Processed Players</h2>
         </header>
 
-        {/* Grid of Processed Player Cards */}
         <ProcessedPlayers
           players={players}
           addItemToShortestQueue={addItemToShortestQueue}
         />
-        {/* <Players players={players} addItemToShortestQueue={addItemToShortestQueue}/> */}
       </div>
     </div>
   );
@@ -357,10 +250,8 @@ function PlayersList({
   addItemToShortestQueue: (id: string) => Queue[];
 }) {
   const unprocessedPlayers = players.filter(
-    // added !player.processedThroughQueue to ensure item doesn't appear in the top area again
     player => !player.assignedToQueue && !player.processedThroughQueue
   );
-  // console.log(unprocessedPlayers);
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {unprocessedPlayers.map(player => (
@@ -389,14 +280,9 @@ function ProcessedPlayers({
   players: Player[];
   addItemToShortestQueue: (id: string) => Queue[];
 }) {
-  // console.log("PLAYERS ********");
-  // console.log(players);
   const processedPlayers = players.filter(player => {
-    // console.log(player)
     return player.processedThroughQueue;
   });
-  // console.log("PROCESSED PLAYERS");
-  // console.log(processedPlayers);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
@@ -428,12 +314,12 @@ function QueuesGrid({
   queues: Queue[];
   onProgress: (index: number) => Queue[];
 }) {
-  // console.log(queues);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       {queues.map((queue, index) => (
         <Queue
           key={queue.id}
+          //REVIEW: move classname directly on the returned div? = fewer props to pass
           className={
             "bg-white rounded-lg shadow-lg p-6 flex flex-col justify-between"
           }
@@ -447,51 +333,34 @@ function QueuesGrid({
   );
 }
 
-// getting ready to implement drag and drop
 // COMPONENT:
 function Queue({queue, setQueues, className, onProgress, index}) {
-  // DRAG N DROP
   const [draggedItem, setDraggedItem] = useState<Player>(null);
-  // don't need to use prevDragged cause we don't care?
   const handleDragStart = draggedItem => setDraggedItem(draggedItem);
   const handleDragOver = (e, targetItem) => e.preventDefault();
 
-  // does the bulk of the drag n drop
+  // does the main dragndrop
   const handleDrop = (e, targetItem) => {
     e.preventDefault();
-
-    console.log("TARGET ITEM FOR DROPPING ");
-    console.log(targetItem);
-    console.log("THE EVENT");
-    console.log(e.target.parentElement);
 
     if (!draggedItem || draggedItem.id === targetItem.id) return;
 
     const draggedIndex = queue.queueItems.findIndex(
       item => item.id === draggedItem.id
     );
-    // console.log("DRAGGED INDEX ", draggedIndex);
-
     const targetIndex = queue.queueItems.findIndex(item => item.id === targetItem.id);
-    // console.log("TARGET INDEX ", targetIndex);
-    // modifying the queue
-    const updatedOrder = [...queue.queueItems];
-    // console.log("UPDATEDORDER before splice ", updatedOrder);
-    // removes the draggeed item (draggedItem - what we move, 1 - how many items are removed)
-    updatedOrder.splice(draggedIndex, 1); //this removes the item and places it where we want
-    // inserts it back without removing any elements (target - where we're moving to; 0 - how many items are removed; draggedItem - what is placed)
-    updatedOrder.splice(targetIndex, 0, draggedItem);
-    // console.log("UPDATEDORDER after splice ", updatedOrder);
 
-    // FIXME: potential cause of the crash when the item is released
+    const updatedOrder = [...queue.queueItems];
+    // removes the draggeed item (draggedItem - what to move, 1 - items to remove)
+    updatedOrder.splice(draggedIndex, 1); //this removes the item and places it where we want
+    // inserts without removing elements (target - where to; 0 - items to remove; draggedItem - what is moved)
+    updatedOrder.splice(targetIndex, 0, draggedItem);
+
     setQueues(prevQueues => {
       return prevQueues.map((q, idx) => {
         if (q.id === queue.id) {
           q.queueItems = updatedOrder;
         }
-        // filter to find the Queue =>
-        // ({...q, updatedOrder})
-        // console.log(q);
         return q;
       });
     });
@@ -505,9 +374,6 @@ function Queue({queue, setQueues, className, onProgress, index}) {
         <h3 className="text-xl font-semibold text-purple-600 mb-4">
           Queue {queue.queueName}
         </h3>
-        <span className="bg-green-300 mx-2 text-white py-2 px-4 rounded">
-          {/* {match?.name} */}
-        </span>
       </div>
       {/* Progress Button */}
       {queue.queueItems.length > 0 && (
@@ -519,10 +385,14 @@ function Queue({queue, setQueues, className, onProgress, index}) {
       )}
       {queue.queueItems.length > 0 ? (
         <ul className="mb-4">
-          {queue.queueItems.map((item, idx) => (
+          {/* REVIEW: item should be a player */}
+          {queue.queueItems.map((item, index) => (
             <QueueItem
-              key={idx}
-              className={"bg-purple-200 text-purple-800 p-2 rounded-lg mb-2"}
+              key={item.id}
+              //REVIEW: move classname directly on the returned div? = fewer props to pass
+              className={`${
+                index === 0 ? "bg-purple-200" : "bg-red-100"
+              }  text-purple-800 p-2 rounded-lg mb-2`}
               item={item}
               onDragStart={handleDragStart}
               onDragOver={handleDragOver}
