fce393d59060097c90875ff296e907b5db027226
Matt McKenzie <matthew.mitchell.mckenzie@gmail.com>
2024-11-08 09:35:38 +1100

    Queue grid number and spacing

--- src/Components/Queue/QueuesGrid.tsx
+++ src/Components/Queue/QueuesGrid.tsx
@@ -7,7 +7,7 @@ import React from "react";
 export default function QueuesGrid() {
   const {queues} = useAppContext();
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 ">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
       {queues.map((queue, index) => (
         <Queue key={queue.id} queue={queue} index={index} />
       ))}
