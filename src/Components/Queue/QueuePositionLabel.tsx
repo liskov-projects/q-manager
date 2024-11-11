bbed29666e7e51646740e3b6a043b17554375d8c
verrakav <verrakav@users.noreply.github.com>
2024-11-11 20:13:21 +1100

    QueuePositionLabel

--- src/Components/Queue/QueuePositionLabel.tsx
+++ src/Components/Queue/QueuePositionLabel.tsx
@@ -1,12 +1,12 @@
-const QueuePositionLabel = ({ index }) => {
-    return (
-      <div className={`text-[0.65rem] font-extrabold mb-1 px-3 py-1 rounded-full ${
+const QueuePositionLabel = ({index}: {index: number}) => {
+  return (
+    <div
+      className={`text-[0.65rem] font-extrabold mb-1 px-3 py-1 rounded-full ${
         index === 0 ? "bg-green-200 text-green-800" : "bg-blue-200 text-blue-800"
       }`}>
-        {index === 0 ? "ON COURT" : `POSITION: ${index}`}
-      </div>
-    );
+      {index === 0 ? "ON COURT" : `POSITION: ${index}`}
+    </div>
+  );
 };
-  
+
 export default QueuePositionLabel;
-  
