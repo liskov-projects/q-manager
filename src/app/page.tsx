b1d3587161026dc546a46840ce6094208337299a
Matt McKenzie <matthew.mitchell.mckenzie@gmail.com>
2024-11-13 21:46:32 +1100

    Quick ChatGPT attempt at mobile styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,50 @@
-"use client";
-// context
-import {AppProvider} from "@/context/AppContext";
-// components
+"use client"
+
+import { useState } from "react";
+import { AppProvider } from "@/context/AppContext";
 import Header from "@/components/Header";
 import SectionHeader from "@/components/SectionHeader";
-// NEW:
 import NewPlayerForm from "@/components/Forms/NewPlayerForm";
 import PlayersList from "@/components/PlayersList";
 import QueuesGrid from "@/components/Queue/QueuesGrid";
 import ProcessedPlayers from "@/components/ProcessedPlayers";
 import ButtonGroup from "@/components/Buttons/ButtonGroup";
-// dummy
-// import Players from "@/Components/Players";
 
 const App = () => {
+  const [visibleSection, setVisibleSection] = useState("queues");
+
   return (
     <AppProvider>
       <Header />
-      <div className="flex flex-row justify-around">
-        <div className="p-2 w-1/4">
+      
+      {/* Mobile toggle button group */}
+      <div className="lg:hidden flex justify-around my-4">
+        <button onClick={() => setVisibleSection("queues")} className="p-2 bg-sky-200 rounded">Queues</button>
+        <button onClick={() => setVisibleSection("unprocessed")} className="p-2 bg-sky-200 rounded">Unprocessed</button>
+        <button onClick={() => setVisibleSection("processed")} className="p-2 bg-sky-200 rounded">Processed</button>
+      </div>
+
+      <div className="flex flex-col lg:flex-row justify-around">
+        {/* Section for adding players and viewing unprocessed list */}
+        <div className={`p-2 w-full lg:w-1/4 ${visibleSection === "unprocessed" ? "block" : "hidden lg:block"}`}>
           <SectionHeader>Add new Players</SectionHeader>
           <NewPlayerForm />
-
           <SectionHeader>Unprocessed Players</SectionHeader>
           <PlayersList />
-          {/* FIXME: trying to make one component for both players groups
-          to do drag n drop */}
-          {/* <Players /> */}
         </div>
 
-        <div className="p-2 w-2/4">
+        {/* Queues section is the main focus */}
+        <div className={`p-2 w-full lg:w-2/4 ${visibleSection === "queues" ? "block" : "hidden lg:block"}`}>
           <SectionHeader>Queues</SectionHeader>
           <QueuesGrid />
         </div>
 
-        <div className="p-2 w-1/4">
+        {/* Processed players section */}
+        <div className={`p-2 w-full lg:w-1/4 ${visibleSection === "processed" ? "block" : "hidden lg:block"}`}>
           <SectionHeader>Button Group</SectionHeader>
           <ButtonGroup />
-          {/* NEW: */}
           <SectionHeader>Processed Players</SectionHeader>
           <ProcessedPlayers />
-          {/* FIXME: trying to make one component for both players groups
-          to do drag n drop */}
-          {/* <Players /> */}
         </div>
       </div>
     </AppProvider>
