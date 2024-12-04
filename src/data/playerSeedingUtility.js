6d91a6afcce0af53e419b596c715aa3eafb9099e
verrakav <verrakav@users.noreply.github.com>
2024-12-04 22:18:55 +1100

    seeds players into Players collection

--- src/data/playerSeedingUtility.js
+++ src/data/playerSeedingUtility.js
@@ -23,6 +23,11 @@ const seedPlayers = async () => {
     console.log("Connected to MongoDB");
 
     // Fetch tournaments from the database
+    const playerCollection = db.collection("players");
+    // Insert players into the database (tournaments)
+    await playerCollection.insertMany(playerSeeds);
+
+    // need await here to do the methods
     const tournamentCollection = await db.collection("tournaments").find().toArray();
     console.log(tournamentCollection);
     if (!tournamentCollection || tournamentCollection.length === 0) {
@@ -56,10 +61,11 @@ const seedPlayers = async () => {
           Math.floor(numPlayers / tournament.queues.length)
         )
       }));
-      // Insert players into the database
+      // Insert players into the database (tournaments)
       await db
         .collection("tournaments")
         .updateOne({_id: tournament._id}, {$set: {queues: updatedQueues}});
+
       console.log(
         `Inserted ${updatedQueues.length} players for tournament: ${tournament.name}`
       );
