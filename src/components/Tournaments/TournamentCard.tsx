e42589bcac8d3e3c2c3bb43a85f07e8763920276
verrakav <verrakav@users.noreply.github.com>
2025-02-03 13:09:08 +0930

    style again

--- src/components/Tournaments/TournamentCard.tsx
+++ src/components/Tournaments/TournamentCard.tsx
@@ -6,23 +6,30 @@ import TournamentCategories from "./TournamentCategories";
 
 export default function TournamentCard({tournament}: {tournament: TTournament}) {
   const {name, _id, description, categories} = tournament;
-  // comes through
-  // console.log(categories);
 
   return (
-    <div className="flex flex-col items-center border border-gray-300 shadow-md rounded-lg p-4 bg-shell-75 transition-all duration-300 hover:shadow-lg hover:-translate-y-4">
-      <Link href={`/all-tournaments/${_id}`}>
+    <div className="w-full min-h-[450px] flex flex-col items-center border border-gray-300 shadow-md rounded-lg p-4 bg-shell-75 transition-all duration-300 hover:shadow-lg hover:-translate-y-2">
+      <Link
+        href={`/all-tournaments/${_id}`}
+        className="flex flex-col items-center w-full h-full">
         <SectionHeader>{name}</SectionHeader>
 
-        <Image
-          //   FIXME:
-          src="/tennis.jpg"
-          alt={`Tournament ${name} Image`}
-          width={350}
-          height={250}
-        />
-        {/* TODO: more styling here */}
-        <div className="text-lg m-2 text-gray-600 font-semibold">{description}</div>
+        {/* Image Container */}
+        <div className="w-[350px] h-[250px]">
+          <Image
+            src="/tennis.jpg"
+            alt={`Tournament ${name} Image`}
+            width={350}
+            height={250}
+            className="object-cover w-full h-full rounded-md"
+          />
+        </div>
+
+        {/* Description & Categories */}
+        <div className="flex-grow text-lg m-2 text-gray-600 font-semibold text-center">
+          {description}
+        </div>
+
         <TournamentCategories categories={categories} />
       </Link>
     </div>
