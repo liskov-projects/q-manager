cf09d77f384dfc40ea70c4acae959ccb8bd076b1
verrakav <verrakav@users.noreply.github.com>
2025-03-25 18:10:04 +0930

    created FavouritePlayersContext

---
+++ src/context/FavouritePlayersContext.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+// TODO: move into types
+type TFavouritePlayersContext = {
+  favourites: string[];
+  toggleFavourite: (playerId: string) => void;
+};
+
+const FavouritePlayersContext = createContext<TFavouritePlayersContext | null>(null);
+
+export function FavouritePlayersProvider({ children }: { children: React.ReactNode }) {
+  const [favourites, setFavourites] = useState<string[]>([]);
+
+  const toggleFavourite = async (playerId: string) => {
+    setFavourites((prev) =>
+      //   if (!prev) return;
+      prev.includes(playerId) ? prev.filter((id) => id !== playerId) : [...prev, playerId]
+    );
+
+    await fetch("api/user/favourites", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ playerId }),
+    });
+  };
+
+  return (
+    <FavouritePlayersContext.Provider value={{ favourites, toggleFavourite }}>
+      {children}
+    </FavouritePlayersContext.Provider>
+  );
+}
+
+export function useFavourites() {
+  const context = useContext(FavouritePlayersContext);
+}
