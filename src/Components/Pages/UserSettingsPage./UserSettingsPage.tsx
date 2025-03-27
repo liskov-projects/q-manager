ea37c1acf96af144f4be2b65854ba41929cc9856
verrakav <verrakav@users.noreply.github.com>
2025-03-27 16:32:26 +0930

    UserSettingsPage layout changes

--- src/Components/Pages/UserSettingsPage./UserSettingsPage.tsx
+++ src/Components/Pages/UserSettingsPage./UserSettingsPage.tsx
@@ -1,6 +1,6 @@
 import { currentUser } from "@clerk/nextjs/server";
 import Favourites from "./Favourites";
-
+import SectionHeader from "@/Components/SectionHeader";
 export default async function UserSettingsPage() {
   const user = await currentUser();
 
@@ -8,12 +8,23 @@ export default async function UserSettingsPage() {
 
   const username = user?.firstName;
   return (
-    <div>
-      <h1>Hello {username}</h1>
-      <h2>Manage Favourites</h2>
-      <span>{`${username}\'s Favourites`}</span>
-      <Favourites />
-      <h2>Manage Notifications</h2>
+    // page container
+    <div className="m-4">
+      {/* header section */}
+      <SectionHeader>{`${username}\'s Dashboard`}</SectionHeader>
+      <span className="text-center">Hello {username} You can manage your favourites here</span>
+      <div className="grid grid-cols-2 gap-4 p-4">
+        <div className="flex flex-col gap-2">
+          <SectionHeader>Manage Favourites</SectionHeader>
+          <Favourites />
+        </div>
+        {/* content section */}
+        <div className="flex flex-col gap-2">
+          <SectionHeader>Manage Notifications</SectionHeader>
+          <span>notification features will be here</span>
+          <span>user data will be here?</span>
+        </div>
+      </div>
     </div>
   );
 }
