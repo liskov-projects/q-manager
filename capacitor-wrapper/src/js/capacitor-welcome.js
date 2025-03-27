63fdc097778d9f2c82089bb7544e50be544ffc24
Matt McKenzie <matthew.mitchell.mckenzie@gmail.com>
2025-03-27 16:26:05 +1100

    Some basic setup for mobile done

--- capacitor-wrapper/src/js/capacitor-welcome.js
+++ capacitor-wrapper/src/js/capacitor-welcome.js
@@ -1,15 +1,15 @@
-import { SplashScreen } from '@capacitor/splash-screen';
-import { Camera } from '@capacitor/camera';
+import { SplashScreen } from "@capacitor/splash-screen";
+import { Camera } from "@capacitor/camera";
 
 window.customElements.define(
-  'capacitor-welcome',
+  "capacitor-welcome",
   class extends HTMLElement {
     constructor() {
       super();
 
       SplashScreen.hide();
 
-      const root = this.attachShadow({ mode: 'open' });
+      const root = this.attachShadow({ mode: "open" });
 
       root.innerHTML = `
     <style>
@@ -92,32 +92,32 @@ window.customElements.define(
     connectedCallback() {
       const self = this;
 
-      self.shadowRoot.querySelector('#take-photo').addEventListener('click', async function (e) {
+      self.shadowRoot.querySelector("#take-photo").addEventListener("click", async function (e) {
         try {
           const photo = await Camera.getPhoto({
-            resultType: 'uri',
+            resultType: "uri",
           });
 
-          const image = self.shadowRoot.querySelector('#image');
+          const image = self.shadowRoot.querySelector("#image");
           if (!image) {
             return;
           }
 
           image.src = photo.webPath;
         } catch (e) {
-          console.warn('User cancelled', e);
+          console.warn("User cancelled", e);
         }
       });
     }
-  },
+  }
 );
 
 window.customElements.define(
-  'capacitor-welcome-titlebar',
+  "capacitor-welcome-titlebar",
   class extends HTMLElement {
     constructor() {
       super();
-      const root = this.attachShadow({ mode: 'open' });
+      const root = this.attachShadow({ mode: "open" });
       root.innerHTML = `
     <style>
       :host {
@@ -138,5 +138,5 @@ window.customElements.define(
     <slot></slot>
     `;
     }
-  },
+  }
 );
--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ console.log("NEXT_PUBLIC_MONGO_URI:", process.env.NEXT_PUBLIC_MONGO_URI);
 console.log("CLERK_PUBLISHABLE_KEY:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
 
 const nextConfig = {
-  output: "standalone", // ✅ Makes the app self-contained for Cloud Run
+  output: "export", // ✅ Makes the app self-contained for Cloud Run
   env: {
     NEXT_PUBLIC_WEBSOCKET_URL: process.env.NEXT_PUBLIC_WEBSOCKET_URL || "",
     MONGO_URI: process.env.NEXT_PUBLIC_MONGO_URI || "", // ✅ Add this
