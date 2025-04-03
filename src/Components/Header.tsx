12abb02e047a54d7abe0f334fb2fa1565c129fb1
verrakav <verrakav@users.noreply.github.com>
2025-04-03 12:47:04 +0930

    heading overlapped the header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,10 +4,9 @@ import { usePathname, useRouter } from "next/navigation";
 // components
 import User from "@/Components/User";
 import Link from "next/link";
-import LiskovLogo from "@/Components/Svgs/LiskovLogo.tsx";
 import Button from "./Buttons/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTable, faTableCells } from "@fortawesome/free-solid-svg-icons";
+import { faTableCells } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   const path = usePathname();
@@ -18,12 +17,8 @@ export default function Header() {
 
   return (
     <header className="w-full flex flex-wrap items-center justify-between gap-4 p-4 sm:flex-nowrap sm:gap-0">
-      {/* Left Section: Logo + Buttons */}
+      {/* Buttons section */}
       <div className="flex items-center gap-2 flex-shrink-0">
-        {/* <div className="w-16 sm:w-20">
-          <LiskovLogo />
-        </div> */}
-
         {notHome && (
           <Link
             href="/all-tournaments"
@@ -44,7 +39,7 @@ export default function Header() {
       </div>
 
       {/* Center Title */}
-      <h1 className="flex-grow text-xl sm:text-2xl text-heading text-center sm:ml-[-4rem]">
+      <h1 className="text-xl sm:text-2xl text-heading text-center sm:ml-[-4rem]">
         Queue Management
       </h1>
 
