97ca6ee661b67b7ebfffe6528423d4284ee44e54
verrakav <verrakav@users.noreply.github.com>
2025-04-07 14:36:40 +0930

    style updates

--- src/Components/Queue/QueuesContainer.tsx
+++ src/Components/Queue/QueuesContainer.tsx
@@ -18,12 +18,12 @@ export default function QueuesContainer() {
         <SectionHeader>Queues</SectionHeader>
         <Button
           onClick={() => setShowAlternateView(!showAlternateView)}
-          className={`mb-4 mx-4 py-1 px-2 ${showAlternateView ? "bg-tennis-200 text-shell-300" : "bg-bluestone-200 text-shell-50"} text-shell-50 hover:text-shell-300 hover:bg-tennis-200 rounded position-center rounded`}
+          className={`mb-4 mx-4 py-1 px-2 ${showAlternateView ? "bg-tennis-200 text-shell-300 border-shell-300 border-2" : "bg-bluestone-200 text-shell-50"} text-shell-50 hover:text-shell-300 hover:bg-tennis-200 rounded position-center rounded`}
         >
           {showAlternateView ? "Show Detailed View" : "Show Grid View"}
         </Button>
         <Button
-          className={`mb-4 mx-4 py-1 px-2 ${showsFavourites ? "bg-tennis-200 text-shell-300" : "bg-bluestone-200 text-shell-50"} text-shell-50 hover:text-shell-300 hover:bg-tennis-200 rounded position-center rounded`}
+          className={`mb-4 mx-4 py-1 px-2 ${showsFavourites ? "bg-tennis-200 text-shell-300 border-shell-300 border-2" : "bg-bluestone-200 text-shell-50"} text-shell-50 hover:text-shell-300 hover:bg-tennis-200 rounded position-center rounded`}
           onClick={() => setShowsFavourites(!showsFavourites)}
         >
           {showsFavourites ? "Show All Players" : "Show Favourites Only"}
